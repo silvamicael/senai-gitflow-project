889cf388431bb2acfa223dee031ecdbda03759ca
silvamicael <micaelmadri@gmail.com>
2026-08-18 20:53:29 -0300

    feat: adiciona criação de tarefas

---
+++ index.js
@@ -0,0 +1,33 @@
+const todos = [];
+
+function addTodo(todo) {
+    todos.push({
+        text: todo,
+        completed: false
+    });
+}
+
+function listTodos() {
+    todos.forEach((todo, index) => {
+        console.log(`${index + 1}. ${todo.text}`);
+    });
+}
+
+function completeTodo(index) {
+    todos[index].completed = true;
+}
+
+function removeTodo(index) {
+    todos.splice(index, 1);
+}
+
+// Dados para teste
+addTodo("Estudar Git");
+addTodo("Fazer atividade");
+addTodo("Criar Pull Request");
+
+completeTodo(1);
+
+removeTodo(3);
+
+listTodos();
